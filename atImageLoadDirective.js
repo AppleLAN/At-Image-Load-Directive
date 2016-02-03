63e7503594b8be315f4db769fe4539ea24d14b83
AppleLAN <alanbuscaglia@gmail.com>
2016-02-02 21:50:17 -0300

    new comments

--- atImageLoadDirective.js
+++ atImageLoadDirective.js
@@ -4,9 +4,9 @@ mainApp.directive('imgLoad', ['$parse', function ($parse) {
     return {
       restrict: 'A',
       link: function (scope, elem, attrs) {
-        var fn = $parse(attrs.imgLoad);
-        elem.on('load', function (event) {
-          scope.$apply(function() {
+        var fn = $parse(attrs.imgLoad); // it parse what we pass as an attr in the html into a function.
+        elem.on('load', function (event) { // The load event is sent to an element when it and all sub-elements have been completely loaded.
+          scope.$apply(function() { //update the bindings, but passing the event first.
             fn(scope, { $event: event });
           });
         });
