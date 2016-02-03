43dbf15c6bf8a97993f99c203527a77d5c42eb1b
AppleLAN <alanbuscaglia@gmail.com>
2016-02-02 22:06:51 -0300

    header :D

--- atImageLoadDirective.js
+++ atImageLoadDirective.js
@@ -1,3 +1,12 @@
+/*
+    _                       __         _____          _       ____  _____  
+     / \                     [  |       |_   _|        / \     |_   \|_   _| 
+    / _ \    _ .--.   _ .--.  | | .---.   | |         / _ \      |   \ | |   
+   / ___ \  [ '/'`\ \[ '/'`\ \| |/ /__\\  | |   _    / ___ \     | |\ \| |   
+ _/ /   \ \_ | \__/ | | \__/ || || \__., _| |__/ | _/ /   \ \_  _| |_\   |_  
+|____| |____|| ;.__/  | ;.__/[___]'.__.'|________||____| |____||_____|\____| 
+            [__|     [__|                                                     */
+
 var mainApp = angular.module("mainApp", []);
 
 mainApp.directive('imgLoad', ['$parse', function ($parse) {
@@ -8,7 +17,7 @@ mainApp.directive('imgLoad', ['$parse', function ($parse) {
         elem.on('load', function (event) { // The load event is sent to an element when it and all sub-elements have been completely loaded.
           scope.$apply(function() { //update the bindings, but passing the event first.
             fn(scope, { $event: event });
-          });
+          });  
         });
       }
     };
